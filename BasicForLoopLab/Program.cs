06b0da6e54a49e7effdfac6da279fc5ba350cbec
morgan <pttrsn.mrgn@gmail.com>
2022-06-29 20:45:08 -0500

    first commit

---
+++ BasicForLoopLab/Program.cs
@@ -0,0 +1,23 @@
+﻿string restart;
+do
+{
+    Console.WriteLine("Enter a number:");
+    int input = int.Parse(Console.ReadLine());
+    int sum = 0;
+    for (int i = 1; i <= input; i++)
+    {
+        sum = i + sum;
+    }
+    Console.WriteLine(sum);
+
+    Console.WriteLine("Would you like to continue? (y/n)?");
+
+    restart = Console.ReadLine().ToLower();
+}
+while (restart == "y");
+
+
+
+Console.WriteLine("Goodbye!");
+
+Thread.Sleep(5000);
